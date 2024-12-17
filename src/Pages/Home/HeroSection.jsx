160008e3682e3022be486ec9012a28e18de41363
Nematullo Nurmamatov <nematullohataboev@gmail.com>
2024-12-18 01:23:18 +0500

    first commit

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,25 @@
+
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Nematullo</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">ReacJS</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            <br /> Dolorum, quas. Amet soluta assumenda cum?
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img className="rasm" src="./img/uzm.jpg" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
